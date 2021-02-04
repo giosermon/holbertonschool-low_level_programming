c5ee56635093a950683c0568add47298b2c1abca
giosermon <2172@holbertonschool.com>
2021-02-04 17:42:42 -0500

    cool_c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * 
+ * return: always 0 (success)
+ */
+int main(void)
+{
+        char alphabt;
+
+        for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+        {
+                if (alphabt != 'q' && alphabt != 'e')
+                    putchar(alphabt);
+        } 
+        putchar('\n');
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
