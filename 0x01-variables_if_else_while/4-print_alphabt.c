32a3ffdab10d2982a2fa3efb07bf45ac0c015fb5
giosermon <2172@holbertonschool.com>
2021-02-04 18:13:36 -0500

    hello_c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main -entry point
- * 
- * return: always 0 (success)
- */
+* main -entry point
+*
+* return: always 0 (success)
+*/
 int main(void)
 {
-        char alphabt;
+    char alphabt;
 
-        for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
-        {
-                if(!(alphabt == 'q' || alphabt == 'e'))
-                    putchar(alphabt);
-        } 
-        putchar('\n');
-        return (0);
+    for (alphabt = 'a'; alphabt <= 'z'; alphabt++)
+{
+    if (!(alphabt == 'q' || alphabt == 'e'))
+    putchar(alphabt);
+}
+putchar('\n');
+return (0);
 }
