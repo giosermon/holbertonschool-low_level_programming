f1a2c0efaea891b4d3d61db1290b468e97eb1d1a
giosermon <2172@holbertonschool.com>
2021-02-09 21:15:37 -0500

    go,go,go_c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always returns (0);
+ */
+
+int main(void)
+{
+int i;
+int sum;
+
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+sum = sum + i;
+}
+}
+printf("%d\n", sum);
+
+return (0);
+}
