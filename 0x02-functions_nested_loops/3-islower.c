9c860db3109270a6cc0a57d07f12c6801f0ca129
giosermon <2172@holbertonschool.com>
2021-02-09 21:06:16 -0500

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,20 +4,19 @@
  * _islower - check for lowercase character.
  * @c: is a character to be check.
  *
- *Return: 1 if c is lowercase. 0 .
+ *Return: 1 if c is lowercase. 0 otherwise.
  */
-
 int _islower(int c)
 {
-    int lwc;
+	int lowercase;
 
-    if (c > 96 && c < 123)
-    {
-            lwc = 1;
-    }
-    else
-    {
-            lwc = 0;
-    }
-    return (lwc);
+	if (c > 96 && c < 123)
+	{
+		lowercase = 1;
+	}
+	else
+	{
+		lowercase = 0;
+	}
+	return (lowercase);
 }
