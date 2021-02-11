c336ad0d1cbb4d876360334ee18ccee157bebb99
giosermon <2172@holbertonschool.com>
2021-02-11 12:22:09 -0500

    this_is-c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * isdigit - check for digits.
+ * @c: is a character to be check.
+ * Return: 1 if c is digit. 0 otherwise.
+ */
+int_isdigit(int c)
+{
+	if (c >= '0' && c <='9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
