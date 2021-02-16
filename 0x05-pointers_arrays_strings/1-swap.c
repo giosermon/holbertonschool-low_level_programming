facc1c8e6b79dbb08b26513691736d5d2978ac9a
giosermon <2172@holbertonschool.com>
2021-02-16 12:44:46 -0500

    hot_fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * swap_int - swap values of two integers.
@@ -8,19 +7,6 @@
  *
  *Return: Always void.
  */
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
-}
-
 void swap_int(int *a, int *b)
 {
 	int swap;
