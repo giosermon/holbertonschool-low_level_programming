f569a105483acf4938dbd3d948a397d3ff7086ca
giosermon <2172@holbertonschool.com>
2021-02-24 16:48:49 -0500

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,20 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - Entry point
- * Desc: Entry
- *@s: pointer
- * Return: Always 0 (Success)
+ * _puts_recursion - function that prints a string.
+ * @s: String literal to print.
+ *
+ * Return: Always void.
  */
-
 void _puts_recursion(char *s)
 {
-    if(*s != '\0')
-    {
-            _putchar(*s);
-            _puts_recursion(s + 1);
-    }
-    else
-    {
-            _putchar('\n');
-    }
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
