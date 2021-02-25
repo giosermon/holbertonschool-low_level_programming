4be286a20bd8a3ae83283d3df3c5a2186ffb72d3
giosermon <2172@holbertonschool.com>
2021-02-24 19:00:32 -0500

    You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *factorial - Entry point
+ *@n: pointer
+ * Return: Always 0 (Success)
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
