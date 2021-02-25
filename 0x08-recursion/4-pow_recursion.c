5d02d04ed0457a3b401ee432cc95350a45bf19b0
giosermon <2172@holbertonschool.com>
2021-02-24 19:11:52 -0500

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,14 +9,18 @@
 
 int _pow_recursion(int x, int y)
 {
-        if (y < 0)
-    {
-            return (-1);
-    }
-        if (y == 0)
-    {
-            return (1);
-}
-
-    return (x * _pow_recursion(x, (y - 1)));
+	/* base case */
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		y--;
+		return (x * _pow_recursion(x, y));
+	}
 }
