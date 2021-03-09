76d736cee027ca821f250f7b98c674fe3fa1e890
giosermon <2172@holbertonschool.com>
2021-03-08 22:29:45 -0500

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,103 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * _strlen - returns the length of a string
+ * new_dog - Adds a new dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
  *
- *@s: character array to count
- *
- * Return: number of characters
+ * Return: dog, a new dog.
  */
-
-int _strlen(char *s)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, c;
+	dog_t *dog;
+	int lname = 0, lown = 0, i = -1; /* WHY? start i = -1 when while(i++) */
 
-	i = 0;
-	c = -1;
-	while (c != 0)
+	if (name == 0 || owner == 0)
+		return (NULL);
+	while (name[lname])
+		lname++;
+	while (owner[lown])
+		lown++;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	dog->name = malloc(lname + 1);
+	if (dog->name)
 	{
-		c = s[i];
-	if (c != 0)
-	{
-		i++;
+		while (i++ < lname)
+			dog->name[i] = name[i];
 	}
-	}
-	return (i);
-}
-
-/**
-* _strcpy - copies the string pointed to by src to the buffer pointed to by
-* dest.
-*
-*@dest: src is copied into here
-*@src: copied into dest
-*
-* Return: pointer to the dest
-*/
-
-	char *_strcpy(char *dest, char *src)
+	else
 	{
-	int i;
-
-	i = 0;
-	while (src[i] != 0)
-	{
-	if (src[i] != 0)
-	{
-	dest[i] = src[i];
-	i++;
+		free(dog);
+		return (NULL);
 	}
-	if (src[i] == 0)
+	i = -1;
+	dog->owner = malloc(lown + 1);
+	if (dog->owner)
 	{
-	dest[i] = '\0';
+		while (i++ < lown)
+			dog->owner[i] = owner[i];
 	}
-	}
-	return (dest);
-	}
-
-	/**
-	 * new_dog - creates a new instance of dog
-	 *
-	 * @name: dog's name
-	 * @age: dog's age
-	 * @owner: dog's owner
-	 *
-	 * Return: always return 0
-	 */
-
-	dog_t *new_dog(char *name, float age, char *owner)
+	else
 	{
-	dog_t *new_dog;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (!new_dog)
-	return (NULL);
-
-	if (name)
-	{
-	new_dog->name = malloc(sizeof(char) * _strlen(name) + 1);
-	if (!new_dog->name)
-	{
-	free(new_dog);
-	return (NULL);
+		free(dog->name);
+		free(dog);
+		return (NULL);
 	}
-	_strcpy(new_dog->name, name);
-	}
-	if (owner)
-	{
-	new_dog->owner = malloc(sizeof(char) * _strlen(owner) + 1);
-	if (!new_dog->owner)
-	{
-	free(new_dog->name);
-	free(new_dog);
-	return (NULL);
-	}
-	_strcpy(new_dog->owner, owner);
-	}
-
-	new_dog->age = age;
-
-	return (new_dog);
+	dog->age = age;
+	return (dog);
 }
