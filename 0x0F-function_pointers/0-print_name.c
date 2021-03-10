fac7663766e7471992304551f5348f043c6de093
giosermon <2172@holbertonschool.com>
2021-03-10 14:38:29 -0500

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <assert.h>
+/**
+ * print_name - prints a name using a pointer function.
+ * @name: name of the person
+ * @f: Pointer function to call functions
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	assert(name != NULL && f != NULL);
+		(*f)(name);
+}
