117c1a75752d4552ac079a3a237febab410f1699
giosermon <2172@holbertonschool.com>
2021-03-10 21:37:47 -0500

    A goal is not always meant to be reached, it often serves simply as something to aim at

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function that sum to integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of sum of two integers.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function that subtract to integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of substraction of two integers.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function that multiplies to integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of multiplication of two integers.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function that divide to integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of division of two integers.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function that give the module of integers.
+ * @a: first integer.
+ * @b: second integer.
+ *
+ * Return: result of module of two integers.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
