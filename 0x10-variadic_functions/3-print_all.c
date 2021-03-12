ce8efec3a92e9ad7a84c8acb05257bd5084f32fa
giosermon <2172@holbertonschool.com>
2021-03-11 21:10:46 -0500

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,86 @@
 #include "variadic_functions.h"
 
 /**
- * printf_char - Print a char.
- * @a: Argument to print.
- *
- * Return: Nothing.
- */
-void printf_char(va_list a)
-{
-	printf("%c", va_arg(a, int));
-}
-
-/**
- * printf_int - Print a int.
- * @a: Argument to print.
- *
- * Return: Nothing.
- */
-void printf_int(va_list a)
-{
-	printf("%d", va_arg(a, int));
-}
-
-/**
- * printf_float - Print a float.
- * @a: Argument to print.
- *
- * Return: Nothing.
- */
-void printf_float(va_list a)
-{
-	printf("%f", va_arg(a, double));
-}
-
-/**
- * printf_string - Print a string.
- * @a: Argument to print.
- *
- * Return: Nothing.
- */
-void printf_string(va_list a)
-{
-	char *s;
-
-	s = va_arg(a, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
-}
-
-/**
- * print_all - Print anything.
- * @format: string with the types to print
- *
- * Return: Nothing.
+ * print_all -  function that prints anything.
+ * @format: arguments
+ * Return: Always 0.
  */
 void print_all(const char * const format, ...)
 {
-	va_list p_list;
-	format_t p_type[] = {
-		{'c', printf_char},
-		{'i', printf_int},
-		{'f', printf_float},
-		{'s', printf_string},
-		{'\0', '\0'}
-	};
-	unsigned int i = 0;
-	unsigned int j = 0;
-	char *s = "";
+cars array[] = {
+{"c", pchar},
+{"i", pint},
+{"f", pfloat},
+{"s", pstring},
+{NULL, NULL},
+};
 
-	va_start(p_list, format);
-	while (format != NULL && format[i] != '\0')
-	{
-		while (p_type[j].type != '\0')
-		{
-			if (format[i] == p_type[j].type)
-			{
-				printf("%s", s);
-				p_type[j].f(p_list);
-				s = ", ";
-			}
-			j++;
-		}
-		j = 0;
-		i++;
-	}
-	printf("\n");
-	va_end(p_list);
+int cf = 0;
+int ca;
+char *string = "";
+
+va_list charl;
+
+va_start(charl, format);
+
+while (format != NULL && format[cf] != '\0')
+{
+ca = 0;
+while (array[ca].argu != NULL)
+{
+if (format[cf] == *array[ca].argu)
+{
+printf("%s", string);
+array[ca].func(charl);
+string = ", ";
+}
+ca++;
+}
+cf++;
+}
+printf("\n");
+va_end(charl);
+}
+/**
+ * pchar - char
+ * @charl: lista de args
+ * Return: void
+ */
+void pchar(va_list charl)
+{
+printf("%c", va_arg(charl, int));
+}
+/**
+ * pint - int
+ * @charl: list of args
+ * Return: void
+ */
+void pint(va_list charl)
+{
+printf("%d", va_arg(charl, int));
+}
+/**
+ * pfloat - float/double
+ * @charl: list of args
+ * Return: void
+ */
+void pfloat(va_list charl)
+{
+printf("%f", va_arg(charl, double));
+}
+/**
+ * pstring - string
+ * @charl: list of args
+ * Return: void
+ */
+void pstring(va_list charl)
+{
+char *s;
+
+s = va_arg(charl, char *);
+if (s == NULL)
+{
+s = "(nil)";
+}
+printf("%s", s);
 }
