7e91a4df8ec50431049bba6b7016b1b280aaf783
giosermon <2172@holbertonschool.com>
2021-03-19 15:49:20 -0500

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
  * print_list - prints list that the main contains
- * @h: pointer to estructure list
+ * @h: puntero a estructura lista
  * Return: number of list's elements
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
+int i = 0;
 
 	while (h != NULL)
 	{
@@ -16,11 +16,10 @@ size_t print_list(const list_t *h)
 	}
 	else
 	{
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 	}
 		i++;
 		h = h->next;
-	return (i);
 	}
-	return (0);
+	return (i);
 }
