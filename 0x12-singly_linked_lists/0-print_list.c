757dd86aac5db65f9248b15bd0ec5b3854da96d3
giosermon <2172@holbertonschool.com>
2021-03-19 15:39:11 -0500

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints list that the main contains
+ * @h: pointer to estructure list
+ * Return: number of list's elements
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+	{
+			printf("[0] (nil)\n");
+	}
+	else
+	{
+			printf("[%d] %s\n", h->len, h->str);
+	}
+		i++;
+		h = h->next;
+	return (i);
+	}
+	return (0);
+}
