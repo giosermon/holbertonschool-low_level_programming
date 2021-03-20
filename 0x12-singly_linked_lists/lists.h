b4c2a887dd8a8c9135babe57fb8e467d74f2aaab
giosermon <2172@holbertonschool.com>
2021-03-19 20:16:47 -0500

    lib

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,6 +28,10 @@ size_t print_list(const list_t *h);
 /*Function that returns the number of elements in a linked list_t list*/
 size_t list_len(const list_t *h);
 
+/*Function that adds a new node at the beginning of a list_t list*/
 list_t *add_node(list_t **head, const char *str);
+
+/*Function that adds a new node at the end of a list_t list*/
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif /*FILE_H*/
