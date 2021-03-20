50a0a717690a20ab4547021cdfe132c4e2feeff5
giosermon <2172@holbertonschool.com>
2021-03-19 22:11:22 -0500

    advance

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_before_main - Function that prints string before main function.
+ *
+ * Return: always nothing.
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
