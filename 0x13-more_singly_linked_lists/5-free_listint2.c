1aa8aecc9551aab30324aea3d997c1ae6b23fdde
giosermon <2172@holbertonschool.com>
2021-03-23 16:40:30 -0500

    free willy

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  free list
+ * @head: list 
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+	}
+}
