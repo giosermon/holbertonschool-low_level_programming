33e8d6206babf7b2e5bc6d26843f3380b85b3c4c
giosermon <2172@holbertonschool.com>
2021-03-23 22:50:01 -0500

    hotfix

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,12 +1,21 @@
 #include "lists.h"
-
 /**
- * reverse_listint - ?
- * @head: ?
- * Return: ?
+ * reverse_listint - Reverse a linked list.
+ * @head: Linked list.
+ * Return: Inverse linked list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	(void) head;
-	return (0);
+	listint_t *current = *head, *temp;
+
+	if (*head == NULL)
+		return (NULL);
+	while (current->next != NULL)
+	{
+		temp = current->next;
+		current->next = temp->next;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (*head);
 }
