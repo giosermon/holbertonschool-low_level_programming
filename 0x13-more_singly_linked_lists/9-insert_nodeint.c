084e54daa2226c37290c7aa9e2a9208c3b22a4b7
giosermon <2172@holbertonschool.com>
2021-03-23 21:23:58 -0500

    Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node
+ * @head: head of list
+ * @idx: inex of elment
+ * @n: value of n of the node
+ * Return: address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *ptr;
+	unsigned int counter;
+
+	if (head)
+	{
+		new = malloc(sizeof(*new));
+		if (new)
+		{
+			new->n = n; /* initialize variable */
+
+			/* include in head */
+			if (idx == 0)
+			{
+				new->next = *head;
+				*head = new;
+				return (new);
+			}
+
+			/* include otherwise */
+			ptr = *head;
+			for (counter = 1; ptr; counter++)
+			{
+				if (counter == idx)
+				{
+					new->next = ptr->next;
+					ptr->next = new;
+					return (new);
+				}
+				ptr = ptr->next;
+			}
+			free(new);
+			return (NULL);
+		}
+		return (NULL);
+	}
+	return (NULL);
+}
