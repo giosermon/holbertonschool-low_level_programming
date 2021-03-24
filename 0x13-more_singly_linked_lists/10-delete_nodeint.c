e77c5dbaf51d858d85fe44d7a4bc7fd3d5e3cf9e
giosermon <2172@holbertonschool.com>
2021-03-23 22:04:34 -0500

    delete

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete node at index of a list in head
+ * @head: head of list
+ * @index: delete node
+ * Return: 1 = SUCCESS, -1 FAIL
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count;
+	listint_t *backup, *ptr;
+
+	if (head && *head)
+	{
+		if (index == 0)
+		{
+			backup = *head;
+			*head = (*head)->next;
+			free(backup);
+			return (1);
+		}
+		ptr = *head;
+		for (count = 1; ptr; count++)
+		{
+			if (count == (index))
+			{
+				backup = ptr;
+				ptr = ptr->next;
+				backup->next = ptr->next;
+				free(ptr);
+				return (1);
+			}
+			ptr = ptr->next;
+		}
+		return (-1);
+	}
+	return (-1);
+}
