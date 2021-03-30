b1e53ecd006d34587f1f87ddb10da77eaedec068
giosermon <2172@holbertonschool.com>
2021-03-29 20:09:25 -0500

    hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	for (i = 0; text_content[i]; i++)
-
+	{}
 	if (filename)
 	{
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
